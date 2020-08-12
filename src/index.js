3219d83f1362498070d9996d7cef61c58b02cf7d
unknown <you@example.com>
2020-08-11 23:34:29 -0300

    GET and POST running!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,57 @@
 const express = require("express");
 
+const { uuid, isUuid } = require("uuidv4");
+
+const { response } = require("express");
 const app = express();
+app.use(express.json());
+
+const devs = [];
 
 const port = 3334;
 
+//array
+
 app.listen(port, () => {
   console.log(`Back-end started! Up on PORT ${port}`);
+});
+
+//GET
+app.get("/devs", (req, res) => {
+  const { firstname } = req.query;
+
+  const results = firstname
+    ? devs.filter((dev) =>
+        dev.firstname.toLowerCase().includes(firstname.toLowerCase())
+      )
+    : devs;
+
+  return res.json(results);
+  //   [
+  // "firstname",
+  // "lastName",
+  // "age",
+  // "company",
+  // "technology",
+  //   ]);
+});
+
+//POST
+app.post("/devs", (req, res) => {
+  const { firstname, lastName, age, company, technology } = req.body;
+  const dev = { id: uuid(), firstname, lastName, age, company, technology };
+
+  devs.push(dev);
+
+  return res.json(dev);
+});
+
+//PUT
+//http://localhost:3334/
+app.put("/devs/:id", (req, res) => {
+  return response.json(["Dev infos..."]);
+});
+
+app.delete("/devs/:id", (req, res) => {
+  return response.json(["dev infos..."]);
 });
